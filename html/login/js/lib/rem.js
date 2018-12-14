3958396cbafd362528c0e1b2567fdde527b8ab9a
hdf123 <15137102387@163.com>
2018-12-14 18:25:01 +0800

    006

--- html/login/js/lib/rem.js
+++ html/login/js/lib/rem.js
@@ -1,13 +1,13 @@
-//$(function(){
-//  window.onload =function(){
-//      getRem(375,100);
-//  };
-//  window.onresize = function(){
-//      getRem(375,100);
-//  };
-//  function getRem(pwidth,prem){
-//      var html = document.getElementsByTagName("html")[0];
-//      var oWidth = document.body.clientWidth || document.documentElement.clientWidth;
-//      html.style.fontSize = oWidth/pwidth*prem+"px";
-//  }
-//})
+$(function(){
+    window.onload =function(){
+        getRem(375,100);
+    };
+    window.onresize = function(){
+        getRem(375,100);
+    };
+    function getRem(pwidth,prem){
+        var html = document.getElementsByTagName("html")[0];
+        var oWidth = document.body.clientWidth || document.documentElement.clientWidth;
+        html.style.fontSize = oWidth/pwidth*prem+"px";
+    }
+})
