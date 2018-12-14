870f18bbf9472bdb51551ba778d5a010486488d8
lvhaichao <2506669531@qq.com>
2018-12-14 18:37:10 +0800

    个人中心提交

--- html/h_store/js/all.js
+++ html/h_store/js/all.js
@@ -1,3 +1,29 @@
+//var baseUrl="http://39.104.181.50:8080/bld/";
+var baseUrl="http://127.0.0.1:8080/bld/";
+
+
+function login() {
+	localStorage.setItem('userId','654321');
+}
+
+function isLogin() {
+	login();//之后去掉
+	var userId=localStorage.getItem('userId');
+    if(userId){
+        userId.replace(/\"/g,"");
+		return userId;
+    }else{
+        window.location.href="./login.html";
+    }
+}
+
+function isEmptyString(obj) {
+	if(typeof obj == "undefined" || obj == null || obj == ""){
+		return true;
+	}else{
+		return false;
+	}
+}
 var go =  function() {
 	window.history.go(-1);
 }
