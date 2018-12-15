fba775bee851d35b2821dddd0602bc1c71357a6f
lvhaichao <2506669531@qq.com>
2018-12-15 13:46:45 +0800

    url工具

--- js/all.js
+++ js/all.js
@@ -1,6 +1,12 @@
 //var baseUrl="http://39.104.181.50:8080/bld/";
 var baseUrl="http://127.0.0.1:8080/bld/";
 
+//获取地址栏后面的信息
+function getUrlString(name) {
+	var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg);
+	if(r!=null)return  unescape(r[2]); return null;
+}
 //ajax调用同步
 function ajaxs(url,type,data,suFn,erFn){
 	$.ajax({
